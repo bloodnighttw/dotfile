4990290584d19f259fd9dd8927e99a7f5efd3324
bloodnighttw <bbeenn1227@gmail.com>
2024-12-29 03:38:42 +0800

    Remove package that is manage by home-manager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -100,15 +100,13 @@
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     curl
-    git
     picom
     sxhkd
-    polybar
+    polybarFull
     rofi
     playerctl
     feh
     home-manager
-    lxappearance
     libappindicator
   ];
 
